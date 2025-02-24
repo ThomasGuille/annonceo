0fe57cabc690cdd5cbc1dca2b90e66fef3bb72ca
Grototo <th.guille@hotmail.com>
2025-02-24 17:17:25 +0100

    HTML CSS JS

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ require_once('include/header.php');
         <div class="sort__dropdown">
             <div class="dropDown">
                 <div class="sort__dropdown__btn" onclick="displayDropdown(this)">
-                    <span class="filter__item">Trier par date de mise en ligne</span>
+                    <span class="filter__item">Trier par date</span>
                     <span class="chevron__box"><i class="fa-solid fa-chevron-down chevron"></i></span>
                 </div>
                 <div class="side__dropdown__link">
@@ -59,6 +59,25 @@ require_once('include/header.php');
                 </div>
             </div>
         </div>
+
+        <hr>
+
+        <div class="card">
+            <div class="picture__box">
+                <img src="assets/images/multimedia/ordinateur-01.jpg" alt="" class="picture">
+            </div>
+            <div class="details">
+                <a href=""><h2 class="details__title">Un truc à vendre</h2></a>
+                <p class="details__text">Un truc tellement formidable que tu vas pas y croire</p>
+                <div class="seller__price">
+                    <h3 class="seller">Jean-Kevin <span class="note">3 / 5</span></h3>
+                    <p class="price">850€</p>
+                </div>
+            </div>
+        </div>
+        <hr>
+
+        <a href="" class="see__more">Voir plus</a>
     </section>
 </main>
     
