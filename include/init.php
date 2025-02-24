6c144d506328470f1209095ca302ed7f4cd75408
Grototo <th.guille@hotmail.com>
2025-02-24 13:34:23 +0100

    BDD

---
+++ include/init.php
@@ -0,0 +1,19 @@
+<?php 
+// Connexion à la BDD
+$dbConnect = new PDO("mysql:host=localhost;dbname=annonceo", "Grototo", "T0n4r1n0r0t0t0!", [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+]);
+
+// Démarrage de la session
+session_start();
+
+// Définition des chemins
+define('RACINE', $_SERVER['DOCUMENT_ROOT'] . '/');
+// echo '<pre>'; print_r(RACINE); echo '</pre>';
+
+define('URL', 'http://localhost:8000/');
+// echo '<pre>'; print_r(URL); echo '</pre>';
+
+
+?>
