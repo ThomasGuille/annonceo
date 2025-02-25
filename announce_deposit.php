89d139c98bbe16d539f06e0dda06579e1d079720
Grototo <th.guille@hotmail.com>
2025-02-25 17:15:08 +0100

    depot annonce (loin de fini)

--- announce_deposit.php
+++ announce_deposit.php
@@ -8,7 +8,47 @@ if(!userConnected()){
 $categoryData = $dbConnect->query("SELECT * FROM category");
 $category = $categoryData->fetchAll(PDO::FETCH_ASSOC);
 
+$categorySelect = $dbConnect->query("SELECT * FROM category WHERE title = '$_POST[category]'");
+$categoryId = $categorySelect->fetch(PDO::FETCH_ASSOC);
+print_r($categoryId);
 
+if(isset($_POST["submit"]) && $_SERVER['REQUEST_METHOD'] === 'POST'){
+    echo '<pre>'; print_r($_POST); echo '</pre>';
+    // echo '<pre>'; print_r($_FILES); echo '</pre>';
+    
+    if(!empty($_FILES["picture1"]["name"])){
+        $allowedExtensions = ["jpg", "jpeg", "png", "webp"];
+        $fileUploaded = new SplFileInfo($_FILES['picture1']['name']);
+        
+        $fileExtension = $fileUploaded->getExtension();
+        // echo $fileExtension . '<br>';
+        $goodExtension = array_search($fileExtension, $allowedExtensions);
+        // echo $goodExtension;
+
+        if($goodExtension === false){
+
+        }else{
+            $pictureName = $_POST["title"] . "-" . $_FILES["picture1"]["name"];
+            $pictureUrl = URL . "assets/images_annonces/$pictureName";
+            $pictureFolder = RACINE . "assets/images-annonces/$pictureName";
+            // echo $pictureFolder;
+            copy($_FILES["picture1"]["tmp_name"], $pictureFolder);
+        }
+    }
+
+    $announceData = $dbConnect->prepare("INSERT INTO annonce VALUES (DEFAULT, $_SESSION[user][id_member], photo $categoryId[id_category], :title, :short_description, :long_description, :price, :photo, :country, :city, :address, :zipcode, NOW())");
+
+    $announceData->bindValue(":title", $_POST["title"], PDO::PARAM_STR);
+    $announceData->bindValue(":short_description", $_POST["shortDesc"], PDO::PARAM_STR);
+    $announceData->bindValue(":long_description", $_POST["longDesc"], PDO::PARAM_STR);
+    $announceData->bindValue(":price", $_POST["price"], PDO::PARAM_STR);
+    $announceData->bindValue(":photo", $pictureUrl, PDO::PARAM_STR);
+    $announceData->bindValue(":country", $_POST["country"], PDO::PARAM_STR);
+    $announceData->bindValue(":city", $_POST["city"], PDO::PARAM_STR);
+    $announceData->bindValue(":address", $_POST["address"], PDO::PARAM_STR);
+    $announceData->bindValue(":zipcode", $_POST["zipcode"], PDO::PARAM_STR);
+    $announceData->execute();
+}
 
 require_once('include/header.php');
 ?>
@@ -16,7 +56,7 @@ require_once('include/header.php');
 
 <main class="main">
     <section class="announce__deposit__main">
-        <form method="post" action="" class="announce__deposit">
+        <form method="post" action="" class="announce__deposit" enctype="multipart/form-data">
             <div class="deposit__fields__block">
                 <div class="deposit__form">
                     <div class="deposit__field">
@@ -53,11 +93,11 @@ require_once('include/header.php');
                 <div class="deposit__form">
                     <div class="deposit__field">
                         <label for="picture" class="deposit__label">Photo (au moins 1 obligatoire)</label>
-                        <input type="file" name="picture1" class="picture__select" placeholder="Titre de l'annonce">
-                        <input type="file" name="picture2" class="picture__select" placeholder="Titre de l'annonce">
-                        <input type="file" name="picture3" class="picture__select" placeholder="Titre de l'annonce">
-                        <input type="file" name="picture4" class="picture__select" placeholder="Titre de l'annonce">
-                        <input type="file" name="picture5" class="picture__select" placeholder="Titre de l'annonce">
+                        <input type="file" name="picture1" class="picture__select">
+                        <input type="file" name="picture2" class="picture__select">
+                        <input type="file" name="picture3" class="picture__select">
+                        <input type="file" name="picture4" class="picture__select">
+                        <input type="file" name="picture5" class="picture__select">
                     </div>
         
                     <div class="deposit__field">
