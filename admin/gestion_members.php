4f541ce6b89f8d2e90c7e7a77bdd0b11ec6cc7eb
Grototo <th.guille@hotmail.com>
2025-02-27 10:39:12 +0100

    backoffice

--- admin/gestion_members.php
+++ admin/gestion_members.php
@@ -1,9 +1,19 @@
 <?php 
 require_once("../include/init.php");
 
-$data = $dbConnect->query("SELECT pseudo, lastName, firstName, phone, email, civility, status, join_date FROM member");
+// echo '<pre>'; print_r($_POST); echo '</pre>';
+
+$data = $dbConnect->query("SELECT id_member, pseudo, lastName, firstName, phone, email, civility, status, join_date FROM member");
 $userData = $data->fetchAll(PDO::FETCH_ASSOC);
 // echo '<pre>'; print_r($userData); echo '</pre>';
+
+if(isset($_POST["submit"])){
+    $statusUpdate = $dbConnect->prepare("UPDATE member SET status = :status WHERE id_member = :id_user");
+    $statusUpdate->bindValue(":status", $_POST["status"], PDO::PARAM_STR);
+    $statusUpdate->bindValue(":id_user", $_POST["id_user"], PDO::PARAM_INT);
+    $statusUpdate->execute();
+    header("location: gestion_members.php");
+}
 
 require_once("include/backheader.php");
 ?>
@@ -25,17 +35,20 @@ require_once("include/backheader.php");
 
             <?php foreach($userData as $user): if($user["status"] == "member"): ?>
                 <tr class="user__table__details">
-                    <?php foreach($user as $key => $value): if($key == "status"): ?>
+                    <?php foreach($user as $key => $value): if($key != "id_member"): if($key == "status"): ?>
                         <td class="user__table__cell">
-                            <select name="status" id="status">
-                                <option <?php if($user["status"] == "admin") echo "selected"; ?> value="admin">Admin</option>
-                                <option <?php if($user["status"] == "member") echo "selected"; ?> value="member">Membre</option>
-                            </select>
+                            <form action="" method="post">
+                                <input name="id_user" type="hidden" value="<?= $user["id_member"]; ?>">
+                                <select name="status" id="status">
+                                    <a href="?action=update&id=<?= $user["id_member"] ?>"><option <?php if($user["status"] == "admin") echo "selected"; ?> value="admin">Admin</option></a>
+                                    <a href="?action=update&id=<?= $user["id_member"] ?>"><option <?php if($user["status"] == "member") echo "selected"; ?> value="member">Membre</option></a>
+                                </select>
+                                <button class="" type="submit" name="submit">OK</button>
+                            </form>
                         </td>
                     <?php else: ?>
                         <td class="user__table__cell"><?= $value ?></td>
-                    <?php endif; endforeach; ?>
-
+                    <?php endif; endif; endforeach; ?>
                 </tr>
             <?php endif; endforeach; ?>
         </table>
@@ -57,16 +70,20 @@ require_once("include/backheader.php");
 
             <?php foreach($userData as $user): if($user["status"] == "admin"): ?>
                 <tr class="user__table__details">
-                    <?php foreach($user as $key => $value): if($key == "status"): ?>
+                    <?php foreach($user as $key => $value): if($key != "id_member"): if($key == "status"): ?>
                         <td class="user__table__cell">
-                            <select name="status" id="status">
-                                <option <?php if($user["status"] == "admin") echo "selected"; ?> value="admin">Admin</option>
-                                <option <?php if($user["status"] == "member") echo "selected"; ?> value="member">Membre</option>
-                            </select>
+                            <form action="" method="post">
+                                <input name="id_user" type="hidden" value="<?= $user["id_member"]; ?>">
+                                <select name="status" id="status">
+                                    <a href="?action=update&id=<?= $user["id_member"] ?>"><option <?php if($user["status"] == "admin") echo "selected"; ?> value="admin">Admin</option></a>
+                                    <a href="?action=update&id=<?= $user["id_member"] ?>"><option <?php if($user["status"] == "member") echo "selected"; ?> value="member">Membre</option></a>
+                                </select>
+                                <button class="" type="submit" name="submit">OK</button>
+                            </form>
                         </td>
                     <?php else: ?>
                         <td class="user__table__cell"><?= $value ?></td>
-                    <?php endif; endforeach; ?>
+                    <?php endif; endif; endforeach; ?>
 
                 </tr>
             <?php endif; endforeach; ?>
